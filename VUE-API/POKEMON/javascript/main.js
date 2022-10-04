f58e42b3037dcbdbdc6a6dc452757208c24bd6e9
gabrod2021 <70831048+gabrod2021@users.noreply.github.com>
2022-10-04 11:56:44 -0300

    Add files via upload

---
+++ VUE-API/POKEMON/javascript/main.js
@@ -0,0 +1,26 @@
+const app=new Vue({
+    el:"#app",
+    data:{
+        info:{}
+        
+     },
+    created() {
+        this.fetchData('https://breakingbadapi.com/api/characters')
+    },
+    methods: {
+        fetchData(url) {        
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.info=data
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+
+                
+            }
+    
+        }
+
+    })
